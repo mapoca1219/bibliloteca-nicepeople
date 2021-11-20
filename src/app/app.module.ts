968e9ee03ff735c02552b3262770cfb80aec407a
Marco <marpolcai@gmail.com>
2021-11-20 12:01:08 -0500

    comienzo del proyecto biblioteca ,agregamos componente listado de libros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListadoLibrosComponent } from './biblioteca/listado-libros/listado-libros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListadoLibrosComponent
   ],
   imports: [
     BrowserModule,
